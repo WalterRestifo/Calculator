51ab21b7e1b2a197a31475e01b53de827795aa0e
Walter Restifo <walterrestifo93@gmail.com>
2022-11-28 22:47:50 +0100

    initial commit and display function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import { useState } from "react";
 
-function App() {
+const calculatorButtons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+export default function App() {
+  const [display, setDisplay] = useState("");
+
+  function handleClick(number) {
+    return setDisplay(display + number);
+  }
+
+  function handleDelete() {
+    return setDisplay("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <StyledHeader>
+        <p>Calculator</p>
+      </StyledHeader>
+      <StyledBody>
+        <StyledInput
+          type={"text"}
+          minLength="10"
+          value={display}
+          placeholder="0"
+        />
+
+        {calculatorButtons.map((numberOfTheButton) => {
+          return (
+            <button
+              key={numberOfTheButton}
+              onClick={() => handleClick(numberOfTheButton)}
+            >
+              {numberOfTheButton}
+            </button>
+          );
+        })}
+        <button onClick={handleDelete}>Delete</button>
+      </StyledBody>
+    </>
   );
 }
 
-export default App;
+const StyledBody = styled.body`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  gap: 1rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+`;
+
+const StyledInput = styled.input`
+  grid-column: span 3;
+`;
+const StyledHeader = styled.header`
+  padding-left: 1rem;
+  padding-right: 1rem;
+`;
